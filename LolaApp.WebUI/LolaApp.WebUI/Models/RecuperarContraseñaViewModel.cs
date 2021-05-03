62fd04cae8bad03267bb38c2a7532d84225417c4
Guillermo Masullo <guimasullo@gmail.com>
2021-05-03 18:45:40 -0300

    recuperar contraseña

---
+++ LolaApp.WebUI/LolaApp.WebUI/Models/RecuperarContraseñaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LolaApp.WebUI.Models
+{
+    public class RecuperarContraseñaViewModel : ModelBase
+    {
+        [Required]
+        [MaxLength(8)]
+        [Display(Name = "Usuario")]// Data Annotation
+        public int Dni { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public int Contraseña { get; set; }
+    }
+}
