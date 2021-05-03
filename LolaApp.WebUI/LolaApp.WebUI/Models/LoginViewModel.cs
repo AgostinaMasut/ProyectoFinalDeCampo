8d3b1ae75203379580af4de0340535c20bd001ce
Agos Masut <agosmasut98@gmail.com>
2021-05-03 17:12:11 -0300

    login page

---
+++ LolaApp.WebUI/LolaApp.WebUI/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LolaApp.WebUI.Models
+{
+    public class LoginViewModel : ModelBase
+    {
+        [Required]
+        [MaxLength(8)]
+        [Display(Name = "Usuario")]// Data Annotation
+        public int Dni { get; set; }
+   
+        [Required]
+        [MaxLength(10)]
+        public int Contraseña { get; set; }
+        
+    }
+}
